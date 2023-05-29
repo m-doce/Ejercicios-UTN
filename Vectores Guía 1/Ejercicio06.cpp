8dd6a2d978058bf4ccdde1d2366389301bfe04d0
Matias Serro <104597265+m-doce@users.noreply.github.com>
2023-05-29 18:18:17 -0300

    Update Ejercicio06.cpp

--- Vectores Guía 1/Ejercicio06.cpp
+++ Vectores Guía 1/Ejercicio06.cpp
@@ -3,39 +3,43 @@ Determinar e imprimir el valor máximo y la posición del mismo dentro del conju
 Si el máximo no es único, imprimir todas las posiciones en que se encuentra.*/
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main()
 {
-    int size;
-    int maximo = -999999;
-    cout<<"Indique el tamaño del vector: "<<endl;
-    cin>>size;
-    int vector[size];
-    std::vector<int> posicionesMaximas; //Declaro un vector sin inicializar la cantidad de posiciones
+    int cantidadElementos;
+    cout<<"Ingrese la cantidad de elementos para el vector: ";
+    cin>>cantidadElementos;
+    int vector[cantidadElementos];
 
-    for(int i=0; i<size; i++)
+    //Lleno el vector con valores que le pido al usuario
+    for(int i=0; i<cantidadElementos; i++)
     {
-        cout<<"Ingrese un numero: "<<endl;
+        cout<<"Ingrese un numero: ";
         cin>>vector[i];
-
-        if(vector[i] > maximo)
-        {
-            maximo = vector[i];
-            posicionesMaximas.clear(); // Limpio las posiciones anteriores
-            posicionesMaximas.push_back(i); // Agrego la nueva posición máxima
-        }
-        else if(vector[i] == maximo)
-        {
-            posicionesMaximas.push_back(i); // Agrego la nueva posición del valor máximo repetido
-        }
     }
 
-    cout<<"El valor maximo es "<<maximo<<" y se encuentra en la/s posicion/es: ";
-    for(int i=0; i<posicionesMaximas.size(); i++)
+    //Creo una variable para guardar el valor más alto del conjunto, y otra para guardar su posición
+    int elMayor = vector[0];
+    int posicion;
+    //Comparo cada posición y reemplazo siempre con el valor más alto
+    for(int i=0; i<cantidadElementos; i++)
     {
-        cout<<posicionesMaximas[i]<<"; ";
+        if(vector[i] > elMayor)
+        {
+            elMayor = vector[i];
+            posicion = i;
+        }
+    }
+    
+    //Imprimo el valor máximo, y luego su/s posición/es
+    cout<<"El valor maximo del conjunto es "<<elMayor<<" y se encuentra en la/s posicion/es: ";
+    for(int i=0; i<cantidadElementos; i++)
+    {
+        if(elMayor == vector[i])
+        {
+            cout<<i<<"; ";
+        }
     }
 
     return 0;
